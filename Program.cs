77bd57a32fc42fafbfdd24697d114785cb019f1f
fasonju <95495174+fasonju@users.noreply.github.com>
2021-12-17 10:57:21 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,181 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Koelkast
+namespace koelkast
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string firstInput = Console.ReadLine();
-            string[] firstInputs = firstInput.Split(' ');
-            uint hoogte = Convert.ToUInt32(firstInputs[1]);
-            uint[] coordinates = new uint[300 * 300];
-            uint t = 0;
-            uint KoenKoelkast = 0;
-            uint endPoint = 0;
-            bool p;
-            if(firstInputs[2] == "P")
-            {
-                p = true;
-            }
-            else
-            {
-                p = false;
-            }
-            for(uint i= hoogte; i>0;i--)
-            {
-                string input = Console.ReadLine();
-                uint width = 1;
-                foreach(char inp in input)
-                {
-                    uint breedte = width << 8;
-                    t++;
+            List<uint> muurlist = new List<uint>();
+            string[] inputarr;
+            
 
-                    if(inp == '+')
-                    {
-                        KoenKoelkast += (i + breedte) << 16;
-                    }
-                    else if(inp == '!')
-                    {
-                        KoenKoelkast += i + breedte;
-                    }
-                    else if(inp == '?')
-                    {
-                        endPoint = i + breedte;
-                    }
-                    else if(inp != '.')
-                    {
-                        coordinates[i + breedte] = 1;
-                    }
-                    width++;
-                }
-            }
-            Queue<uint> queue = new Queue<uint>();
-            queue.Enqueue(KoenKoelkast);
-            Dictionary<uint, uint> processed = new Dictionary<uint, uint>();
-            long lastValue = -1;
-            uint NorthKoen = 1 << 16;
-            uint EastKoen = 1 << 24;
+            inputarr = Console.ReadLine().Split(' ');
+            uint b = UInt32.Parse(inputarr[0]);
+            uint h = UInt32.Parse(inputarr[1]);
+            string m = inputarr[2];
+            uint cc = 0;
+            uint einddoel;
+            
 
-            uint NorthKoel = 1;
-            uint EastKoel = 1 << 8;
+            List<uint> wallist = new List<uint>();
 
-            processed.Add(KoenKoelkast, KoenKoelkast);
-            while(queue.Count != 0)
+            for(uint i = h; i > 0; i--)
             {
-                uint newValue = queue.Dequeue();
-                if(((newValue + NorthKoen) >> 16) == (newValue - ((newValue >> 16)<<16)) && coordinates[(newValue - ((newValue >> 16) << 16)) + NorthKoel] != 1 && !processed.ContainsKey(newValue+NorthKoen+NorthKoel)) //check if hit koelkast and if wall next to koelkast in direction X and if state is a dictionary.
+                string plat = Console.ReadLine();
+                uint width = 0;
+
+                foreach (char x in plat)
                 {
-                    uint inputValue = newValue + NorthKoen + NorthKoel;
-                    processed.Add(inputValue, newValue);
-                    queue.Enqueue(inputValue); 
-                    if(inputValue - ((inputValue >> 16) << 16) == endPoint)
+                    if (x == '!')
                     {
-                        lastValue = inputValue;
-                        break;
+                        cc += (width << 24) + (i << 16);
                     }
-                }
-                else if(coordinates[(newValue + NorthKoen) >> 16] != 1 && !processed.ContainsKey(newValue + NorthKoen) && ((newValue + NorthKoen) >> 16) != (newValue - ((newValue >> 16) << 16))) //wall next to koelkast or wall next to you in direction X and if state in dictionary.
-                {
-                    uint inputValue = newValue + NorthKoen;
-                    processed.Add(inputValue, newValue);
-                    queue.Enqueue(inputValue);
-                }
-                if (((newValue - NorthKoen) >> 16) == (newValue - ((newValue >> 16) << 16)) && coordinates[(newValue - ((newValue >> 16) << 16)) - NorthKoel] != 1 && !processed.ContainsKey(newValue - NorthKoen - NorthKoel)) //check if hit koelkast and if wall next to koelkast in direction X and if state is a dictionary.
-                {
-                    uint inputValue = newValue - NorthKoen - NorthKoel;
-                    processed.Add(inputValue, newValue);
-                    queue.Enqueue(inputValue);
-                    if (inputValue - ((inputValue >> 16) << 16) == endPoint)
+                    else if(x == '?')
                     {
-                        lastValue = inputValue;
-                        break;
+                        einddoel = (width << 8) + (i);
                     }
-                }
-                else if (coordinates[(newValue - NorthKoen) >> 16] != 1 && !processed.ContainsKey(newValue - NorthKoen) && ((newValue -  NorthKoen) >> 16) != (newValue - ((newValue >> 16) << 16))) //wall next to koelkast or wall next to you in direction X and if state in dictionary.
-                {
-                    uint inputValue = newValue - NorthKoen;
-                    processed.Add(inputValue, newValue);
-                    queue.Enqueue(inputValue);
-                }
-                if (((newValue + EastKoen) >> 16) == (newValue - ((newValue >> 16) << 16)) && coordinates[(newValue - ((newValue >> 16) << 16)) + EastKoel] != 1 && !processed.ContainsKey(newValue + EastKoen + EastKoel)) //check if hit koelkast and if wall next to koelkast in direction X and if state is a dictionary.
-                {
-                    uint inputValue = newValue + EastKoen + EastKoel;
-                    processed.Add(inputValue, newValue);
-                    queue.Enqueue(inputValue);
-                    if (inputValue - ((inputValue >> 16) << 16) == endPoint)
+                    else if(x == '+')
                     {
-                        lastValue = inputValue;
-                        break;
+                        cc += (width << 8) + (i);
                     }
-                }
-                else if (coordinates[(newValue + EastKoen) >> 16] != 1 && !processed.ContainsKey(newValue + EastKoen) && ((newValue + EastKoen) >> 16) != (newValue - ((newValue >> 16) << 16))) //wall next to koelkast or wall next to you in direction X and if state in dictionary.
-                {
-                    uint inputValue = newValue + EastKoen;
-                    processed.Add(inputValue, newValue);
-                    queue.Enqueue(inputValue);
-                }
-                if (((newValue - EastKoen) >> 16) == (newValue - ((newValue >> 16) << 16)) && coordinates[(newValue - ((newValue >> 16) << 16)) - EastKoel] != 1 && !processed.ContainsKey(newValue - EastKoen - EastKoel)) //check if hit koelkast and if wall next to koelkast in direction X and if state is a dictionary.
-                {
-                    uint inputValue = newValue - EastKoen - EastKoel;
-                    processed.Add(inputValue, newValue);
-                    queue.Enqueue(inputValue);
-                    if (inputValue - ((inputValue >> 16)<<16) == endPoint)
+                    else if(x != '.')
                     {
-                        lastValue = inputValue;
-                        break;
+                        wallist.Add((width << 8) + (i));
                     }
+
+                    width += 1;
                 }
-                else if (coordinates[(newValue - EastKoen) >> 16] != 1 && !processed.ContainsKey(newValue - EastKoen) && ((newValue - EastKoen) >> 16) != (newValue - ((newValue >> 16) << 16))) //wall next to koelkast or wall next to you in direction X and if state in dictionary.
-                {
-                    uint inputValue = newValue - EastKoen;
-                    processed.Add(inputValue, newValue);
-                    queue.Enqueue(inputValue);
-                }
+                
             }
-            if(lastValue != -1)
+            
+            Console.ReadKey();
+
+        }
+
+        public static void pathfinder(uint cc, List<uint> wallist, uint einddoel)
+        {
+            Queue<uint> myqu = new Queue<uint>();
+            myqu.Append(cc);
+            List<uint> pcc = new List<uint>();
+            pcc.Add(cc);
+            while(myqu.Count != 0)
             {
-                uint key = Convert.ToUInt32(lastValue);
-                uint value;
-                string theWay = "";
-                int lengthPath = 0;
-                while(true)
+                uint northcc = direction(cc, 1, wallist, einddoel);
+                if (pcc.Contains(northcc) == false)
                 {
-                    value = processed[key];
-                    if (((value >> 16) - ((value >> 24) << 8)) + 1 == (key>>16) - ((key>>24) << 8))
-                    {
-                        theWay = "N" + theWay;
-                    }
-                    if (((value >> 16) - ((value >> 24) <<8)) - 1 == (key >> 16) - ((key >> 24)<<8))
-                    {
-                        theWay = "S" + theWay;
-                    }
-                    if ((value >> 24) + 1 == key>>24)
-                    {
-                        theWay = "E" + theWay;
-                    }
-                    if ((value >> 24) - 1 == key>>24)
-                    {
-                        theWay = "W" + theWay;
-                    }
-                    if (key - value == 0)
-                    {
-                        break;
-                    }
-                    key = value;
-                    lengthPath++;
+                    pcc.Add(northcc);
+                    myqu.Append(northcc);
                 }
-                Console.WriteLine(lengthPath);
-                if (p)
-                {
-                    Console.WriteLine(theWay);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No solution");
+                
             }
         }
+
+        public static uint direction(uint cc,uint direction, List<uint> wallist, uint einddoel)
+        {
+            switch(direction)
+            {
+                case 1:
+                    uint northcc = cc + (1 << 16);
+                    var coen = ((northcc >> 16) << 16);
+                    if ( northcc - coen == einddoel)
+                    {
+                        //bereikt
+                    }
+                    else if(wallist.Contains(coen) == true)
+                    {
+                        //niet in qu doen
+                    }
+                    else if (wallist.Contains(northcc - coen) == true)
+                    {
+                        //niet in qu doen
+                    }
+                    else
+                    {
+                        return northcc;
+                    }
+
+                    break;
+            }
+        }
+       
     }
 }
